4cd382aa2250be91e57f0d2739ad6e5c4a3ffb48
shoebilyas123 <shoebilyas123@gmail.com>
2024-06-10 14:13:56 +0530

    remote setup

---
+++ remote/src/Button.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+import './Button.css';
+
+export const Button = () => {
+  const [state, setState] = useState(0);
+  return (
+    <div>
+      <button className="shared-btn" onClick={() => setState((s) => s + 1)}>
+        Click me: {state}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
